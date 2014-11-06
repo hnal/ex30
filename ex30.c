b79732fe687d13d7ee2e958321d758341c17cb15
Raimundofelipe <raimundo_felipe@yahoo.com.br>
2014-11-06 12:33:15 -0300

    adicionei minha funcao

--- ex30.c
+++ ex30.c
@@ -1,26 +1,76 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-int rfpdn(int x);
-
-=======
+int caio(int c);
+int rodrigo(int a);
+int calado(int k);
+int nmrn(int x);
 int wesley(int x);//declaração da função Wesley entrada inteira x
-	
->>>>>>> 2c00711749dd5c163c87cc7b1bfaabcc4e68048f
+int ruben(int x);	
+int vinicius(int x);
+int regina(int x);
+int victor(int x);
+int raimundo(int x);
+
 int main(void)
 {
-	;
+	int i=1;
+	i = nmrn(i);
+	i = wesley(i);
+	i = ruben(i);
+	i = calado(i);
+	i = caio(i);
+	i = rodrigo(i);
+	i = vinicius(i);
+	printf ("o prgrama termina aqui \n");
+	return 0;
 }
 
-<<<<<<< HEAD
-int rfpdn(int x)
+int calado(int k)
+{
+		return k+3;
+}
+
+int nmrn(int x)
+{	
+		return x-1;
+}
+
+int wesley(int x)//função wesley, recebe initeiro
+{
+		        return x*157; //retorna o numero inteiro de entrada multiplicado a 157
+}
+
+int vinicius(int x)
+{
+		return x=1;
+}
+
+int ruben(int x)
+{
+		return x+1;
+
+}
+
+int caio(int c)
+{
+		return c+10;
+}
+
+int rodrigo(int a)
+{
+		return a+6;
+}
+
+int regina(int x)
+{
+		return x+23;
+}
+int victor(int x)
+{
+		return x+x;
+}
+
+int raimundo(int x)
 {
 	return x+1;
 }
-=======
-int wesley(int x)//função wesley, recebe initeiro
-{
-	        return x*157; //retorna o numero inteiro de entrada multiplicado a 157
-}
-
->>>>>>> 2c00711749dd5c163c87cc7b1bfaabcc4e68048f
