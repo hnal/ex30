2c00711749dd5c163c87cc7b1bfaabcc4e68048f
Wesley Gabriel Cardozo dos Santos <gabriel_head_2@hotmail.com>
2014-11-06 10:01:15 -0300

    Funcao wesley finalizada

--- ex30.c
+++ ex30.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int wesley(int x)//declaração da função wesley entrada inteira x
+int wesley(int x);//declaração da função Wesley entrada inteira x
 	
 int main(void)
 {
@@ -9,6 +9,6 @@ int main(void)
 
 int wesley(int x)//função wesley, recebe initeiro
 {
-	        return x*157 //retorna o numero inteiro de entrada multiplicado a 157
+	        return x*157; //retorna o numero inteiro de entrada multiplicado a 157
 }
 
